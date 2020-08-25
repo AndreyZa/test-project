c682560c2f960338d19fc31bc0b248c09adaf51d
Andrey Zaitsev <andreyzaitsev31@gmail.com>
2020-08-25 23:07:49 +0300

    Removed service worker.Added and configured Prettier and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
